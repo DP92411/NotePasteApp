be108f3d2a20a9a135936fb44e5fc2a7d136dac0
DP92411 <168977438+DP92411@users.noreply.github.com>
2025-01-06 19:24:27 +0530

    Update main.jsx
    imported toast

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
 import  { Toaster } from 'react-hot-toast';
+import { toast } from 'react-hot-toast';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
